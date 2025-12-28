d70fa55425b0abde794319ae6fe3492ec6074c09
XVN5002036 <89866299+xvn5002036@users.noreply.github.com>
2025-12-28 19:01:48 +0800

    Create rank.js

---
+++ constants/rank.js
@@ -0,0 +1,10 @@
+export const RANK_SYSTEM = {
+  "DU_GONG": { level: "正一都功", office: "都功署" },
+  "MENG_WEI": { level: "正一盟威", office: "盟威署" },
+  "WU_LEI": { level: "五雷顯應", office: "五雷院" }
+};
+
+export const GENDER_TITLE = {
+  "MALE": { suffix: "乾道", taoist_title: "先生" },
+  "FEMALE": { suffix: "坤道", taoist_title: "元君" }
+};
