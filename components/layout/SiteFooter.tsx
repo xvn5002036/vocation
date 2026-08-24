0a8a97ee28ed42c0f5242e2e774670408f4993d3
XVN5002036 <89866299+xvn5002036@users.noreply.github.com>
2026-08-25 01:27:27 +0800

    deploy: publish website

--- components/layout/SiteFooter.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SiteFooter: React.FC = () => (
-  <footer className="site-footer"><span>龍虎山正一 · 授籙管理系統</span><span>敬天法祖 · 依格奉行</span></footer>
-);
-
-export default SiteFooter;
